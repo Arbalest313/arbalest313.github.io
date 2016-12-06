a1ab5c9099110d3342cbd381c65ec23e86065f3c
huangyuan@tuhu.cn <huangyuan@tuhu.cn>
2016-12-06 16:25:36 +0800

    调试多说样式

---
+++ _site/js/hYDev.js
@@ -0,0 +1,12 @@
+function hYDev() {
+	$(".ds-meta").prependTo(".ds-comments-info");
+	$(".ds-comments-tabs").appendTo(".ds-meta");
+	$(".ds-comments-tabs").css("float", "left");
+	$(".ds-comments-tab-duoshuo").css("font-size", "13.5px");
+
+};
+$(window).scroll(function() {
+	hYDev()
+});
+
+// $(document).ready(hYDev());
